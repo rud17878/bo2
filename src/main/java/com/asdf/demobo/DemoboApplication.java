84b8daa29278b9d950fffad72568c1a6c9fd6722
rud17878 <rud178qwer@gmail.com>
2020-06-11 09:26:35 +0900

    add local project

---
+++ src/main/java/com/asdf/demobo/DemoboApplication.java
@@ -0,0 +1,13 @@
+package com.asdf.demobo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asdf/demobo/DemoboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asdf.demobo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
